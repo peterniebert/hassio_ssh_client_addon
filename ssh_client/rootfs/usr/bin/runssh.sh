5da62de45574d692d16ce95865e44a055cc9a107
Peter Niebert <peter.niebert@univ-amu.fr>
2023-12-19 19:34:31 +0100

    WIP

--- ssh_client/rootfs/usr/bin/runssh.sh
+++ ssh_client/rootfs/usr/bin/runssh.sh
@@ -13,10 +13,11 @@
 main() {
 
     bashio::log.info "Install private key from config"
-
+    mkdir -p /root/.ssh
     echo  $(bashio::config 'ssh_private_key') > /root/.ssh/mykey
     echo  $(bashio::config 'ssh_server_public_key') > /root/.ssh/known_hosts
-
+    chmod 600 /root/.ssh/mykey
+    chmod 600 /root/.ssh/known_hosts
 
     while true; do
         bashio::log.info "starting ssh client"
