af8a170a4273f27500782ae93f35dcb582137283
Peter Niebert <peter.niebert@univ-amu.fr>
2023-12-20 06:45:10 +0100

    WIP

--- ssh_client/rootfs/usr/bin/runssh.sh
+++ ssh_client/rootfs/usr/bin/runssh.sh
@@ -14,20 +14,15 @@ main() {
     export HOME=/root
     bashio::log.info "Install private key from config"
     mkdir -p /root/.ssh
-    privkey=$(bashio::config 'ssh_private_key')
-    echo  $privkey > /root/.ssh/mykey
-    servkey=$(bashio::config 'ssh_server_public_key')
-    echo  $servkey > /root/.ssh/known_hosts
-    chmod 600 /root/.ssh/mykey
-    chmod 600 /root/.ssh/known_hosts
+    cp /addons/ssh-client/* /root/.ssh/
+    chmod 600 /root/.ssh/*
+    privkey=/root/.ssh/$(bashio::config 'ssh_private_key')
 
     more_args=$(bashio::config 'ssh_more_args')
     server_url=$(bashio::config 'ssh_remote_url')
     server_port=$(bashio::config 'ssh_remote_port')
-    while true; do
-        bashio::log.info "starting ssh client"
-        echo /usr/bin/ssh -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o ExitOnForwardFailure=yes   $more_args $server_url -N -p $server_port -i /root/.ssh/mykey
-        sleep 5
-    done
+    bashio::log.info "starting ssh client"
+    echo /usr/bin/ssh -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o ExitOnForwardFailure=yes   $more_args $server_url -N -p $server_port -i /root/.ssh/mykey
+    /usr/bin/ssh -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o ExitOnForwardFailure=yes   $more_args $server_url -N -p $server_port -i /root/.ssh/mykey
 }
 main "$@"
